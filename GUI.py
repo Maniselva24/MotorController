9e53c3b60d98943856d2a1ec870aa5caa70e6d4d
Manikandan <manikandanselvaraj.gd @gmail.com>
2023-12-28 15:25:49 +0530

    Add TCP Client script

--- GUI.py
+++ GUI.py
@@ -1,18 +1,43 @@
-# This is a sample Python script.
+import tkinter as tk
+from tkinter import ttk
+from TCP_Client import tcp_com
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# root window
+root = tk.Tk()
+root.geometry('300x200')
+root.resizable(False, False)
+root.title('Button Demo')
+input_var = "hello"
+L1 = ttk.Label(root, text="Data to Send")
+L1.pack(
+    ipadx=1,
+    ipady=1,
+    expand=True
+)
+E1 = tk.Entry(root, textvariable=input_var, font=('calibre', 10, 'normal'))
+E1.pack(
+    ipadx=1,
+    ipady=1,
+    expand=True
+)
 
 
-import tkinter
+def on_click():
+    tcp_com(E1.get())
+    print(E1.get())
 
-window = tkinter.Tk()
-window.title("MotorX")
-window.minsize(width=500, height=300)
 
-# label
-label_title = tkinter.Label(text="Motor Controller")
-label_title.pack()
+# exit button
+exit_button = ttk.Button(
+    root,
+    text='Start',
+    command=on_click
+)
 
-# should be end of the program
-window.mainloop()
+exit_button.pack(
+    ipadx=2,
+    ipady=2,
+    expand=True
+)
+
+root.mainloop()
---
+++ TCP_Client.py
@@ -0,0 +1,18 @@
+# tcp client
+
+import socket
+
+
+def tcp_com(x):
+    host = "192.168.1.9"  # The server's hostname or IP address
+    port = 1336  # The port used by the server
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.settimeout(5)
+        try:
+            s.connect((host, port))
+            s.sendall(bytes(x, encoding='utf-8'))
+            # data = s.recv(8)    # will receive 8 bytes
+            s.close()
+        except socket.error as exc:
+            print("Caught exception socket.error : %s" % exc)
+# print(f"Received {data!r}")
